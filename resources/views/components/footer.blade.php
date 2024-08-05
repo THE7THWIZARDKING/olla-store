aa24babff8a69456af1755e6c022f481ac7edb49
james shrestha <jameshrestha07@gmail.com>
2024-08-05 14:28:51 +0545

    created navbar till now

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-[#0F172A] shadow-md py-6 mt-16">
+    <div class="container mx-auto text-center">
+        <p class="text-white">&copy; 2024 olla shop. All rights reserved.</p>
+    </div>
+</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,6 @@
+<nav class="bg-[#0F172A] text-white flex justify-left px-20 py-3 gap-12">
+    <a href="/">Home</a>
+    <a href="/service">Service</a>
+    <a href="/about-us">About</a>
+    <a href="/contact">Contact</a>
+</nav>
