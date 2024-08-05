08434e6732019ec43112b115ce11562d6bc76ccf
james shrestha <jameshrestha07@gmail.com>
2024-08-05 14:38:35 +0545

    minor  chnages on footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-[#4F46E5] shadow-md py-6 mt-16">
     <div class="container mx-auto text-center">
-        <p class="text-white">&copy; 2024 olla shop. All rights reserved.</p>
+        <p class="text-white">&copy; 2024 Olla shop. All rights reserved. since 2024.</p>
     </div>
 </div>
